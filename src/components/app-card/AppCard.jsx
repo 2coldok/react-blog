072aa35d7a2ebc42e46de0ecede4bc7f915fa656
coldok <2coldok@gmail.com>
2024-03-09 00:31:15 +0900

    image deploy test

--- src/components/app-card/AppCard.jsx
+++ src/components/app-card/AppCard.jsx
@@ -2,11 +2,12 @@ export default function AppCard({ love }) {
   const { id, name, img, path } = love;
 
   // const imgURL = `https://내아이디.github.io/리포지토리명/app-image/${path}.png`;
+  console.log(img);
   
   return (
     <>
       <div>appcard 에용</div>
-      <img src={`https://2coldok.github.io/react-blog/image/${path}.png`} alt="oh"/> 
+      <img src={`https://2coldok.github.io/react-blog/image/${img}.png`} alt="oh"/> 
     </>
   );
 }
